01d2e054ca5b1f0ccd8b059f6c0829cd0b0036eb
martin_dang <martin.dang@sirma.bg>
2018-04-24 17:51:36 +0300

    update versions

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
+  name: "martindan:sparkline",
   summary: "Simple jquery.sparkline package",
   version: "1.3.1",
-  git:"https://github.com/rollymaduk/jqsparkline.git"
+  git:"https://github.com/martindan/jqsparkline.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.0.1');
+  api.versionsFrom('METEOR@1.6.1');
   api.use(['jquery','underscore']);
-  api.addFiles('sparkline.html','client');
+  api.addAssets('sparkline.html','client');
   api.addFiles('lib/jquery.sparkline.js','client');
 });
 
